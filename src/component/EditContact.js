1d5daec8cc5a3b33d30b0e21bacced2a92dee5cc
Eximias Developer <eximias@Eximiass-MacBook-Pro.local>
2022-10-13 16:58:24 +0700

    first commit

---
+++ src/component/EditContact.js
@@ -0,0 +1,54 @@
+import React,{useState} from 'react';
+import './App.css'
+
+
+function AddContact(props) {
+    const initialValues = {name: "", mail: ""}
+    const [formValues, setFormValues] = useState(initialValues)
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormValues({ ...formValues, [name]: value})
+    }
+    console.log(formValues)
+    const handleAdd = (e) => {
+        e.preventDefault()
+        if (formValues.name === "" || formValues.mail === "") {
+            alert("Don't Empty")
+            return
+        }
+        props.addContact(formValues)
+        setFormValues(initialValues)
+    }
+
+
+    return (
+        <div className={"container-form"}>
+            <form onSubmit={handleAdd}>
+                <h3>Add Contact</h3>
+                <div className={'container-input'}>
+                    <input
+                        type="text"
+                        placeholder="Your name.."
+                        name={"name"}
+                        value={formValues.name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Your mail.."
+                        name={"mail"}
+                        value={formValues.mail}
+                        onChange={handleChange}
+                    />
+
+                    <button>Add</button>
+                </div>
+            </form>
+        </div>
+    );
+
+}
+
+export default AddContact;
